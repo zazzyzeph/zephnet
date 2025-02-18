46a107b8b5e18e76916d16f6a2b7184a63160d76
Zephyr Prusinski <zephyr@zephnet.biz>
2025-02-18 08:14:58 -0500

    api testing

--- functions/micropub.js
+++ functions/micropub.js
@@ -4,7 +4,19 @@ export function onRequestGet(context) {
 
 export function onRequestPost(context) {
   if (context.env.DEV){
-    return new Response(JSON.stringify(context))
+    let template = `
+---
+title: "First"
+type: "posts"
+date: 2024-12-28T14:50:07-05:00
+draft: false
+featured_image: false
+featured_image_alt: false
+---
+
+content content content
+    `
+    return new Response(JSON.stringify(context.request.body)
   }
   return new Response("request denied")
 }
