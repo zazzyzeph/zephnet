6ff4079424fa21f8a279e2e6799d8967ccd1163d
Zephyr Prusinski <zephyr@zephnet.biz>
2025-06-15 22:23:06 -0400

    github api working but commented to work on authorization

--- archetypes/default.md
+++ archetypes/default.md
@@ -6,9 +6,11 @@ featured_image: false
 featured_image_alt: false
 params:
     likes:
+        total = 0
         bluesky = 0
         mastodon = 0
     comments:
+        total = 0
         bluesky = 0
         mastodon = 0
 ---
--- functions/micropub.js
+++ functions/micropub.js
@@ -1,24 +1,16 @@
 import { githubCommitFromAuthenticatedPost } from "./src/github_commit.js";
 
-const yaml = require("js-yaml");
-const fs = require("fs");
-
 // i mentioned in my /about page that i would take the address of the micropub address to my grave.
 // funny message 4 the hackers
 export async function onRequestGet(context) {
-  const { request, env } = context;
-  try {
-    const doc = yaml.load(fs.readFileSync("../config.yaml", "utf8"));
-  } catch (e) {
-    console.log(e);
-  }
-  return new Response(doc);
+  return new Response("How did you get this address");
 }
 
 // handle any POST request in this function
 export async function onRequestPost(context) {
   // split out the request and env objects from the context object with destructive assignment
   const { request, env } = context;
-  const response = githubCommitFromAuthenticatedPost(request, env);
+
+  // const response = githubCommitFromAuthenticatedPost(request, env);
   return response;
 }
