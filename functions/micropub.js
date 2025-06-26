9178da087d4aecb0827c22c14785c666be8345cb
Zephyr Prusinski <zephyr@zephnet.biz>
2025-06-26 00:18:59 -0400

    dropped env from indieauth func, added image support but still testing

--- functions/micropub.js
+++ functions/micropub.js
@@ -1,6 +1,7 @@
 import { githubCommitFromAuthenticatedPost } from "./src/github_commit.js";
 import { authorizationTokenVerification } from "./src/validate_indieauth_token.js";
 import { generatePostMarkdown } from "./src/generate_post_markdown.js";
+import { imagesToUrls } from "./src/images.js";
 
 // i mentioned in my /about page that i would take the address of the micropub address to my grave.
 // funny message 4 the hackers
@@ -13,23 +14,47 @@ export async function onRequestPost(context) {
   // split out the request and env objects from the context object with destructive assignment
   const { request, env } = context;
 
-  const headers = new Headers();
-
   // Resource is a ReadableStream, with the contents being a url param string
   const formData = await request.formData();
 
+  // token can either come as a field in the body (access_token=>XXXXXXX)
+  // or in the Authorization header (Bearer XXXXXXXXXX)
+  let token = "";
+  const formDataToken = formData.get("access_token");
+  const headerToken = request.headers.get("Authorization");
+  token = formDataToken ?? "";
+  if (!token && headerToken) {
+    const splitArr = headerToken.split("Bearer ");
+    if (splitArr.length > 1) {
+      token = splitArr[1];
+    }
+  }
+
+  if (!token) {
+    return new Response("Not Authorized >:^(", {
+      status: 403,
+    });
+  }
+
   // make sure we have the bare minimum for a a post (token, content (or image + alt), object type being created (h=entry probably))
-  const requiredKeysArrText = ["access_token", "content", "h"];
-  const requiredKeysArrImage = ["access_token", "photo", "h"];
+  const requiredKeysArrText = ["content", "h"];
+  const requiredKeysArrImage = ["photo[]", "h"];
   const hasRequiredKeysText = requiredKeysArrText.every((item) =>
     formData.has(item),
   );
   const hasRequiredKeysImage = requiredKeysArrImage.every((item) =>
     formData.has(item),
   );
+  let keys = "";
+  let keyArr = formData.keys();
+  for (const key of keyArr) {
+    keys += " " + key;
+  }
 
   if (!hasRequiredKeysText && !hasRequiredKeysImage) {
-    return new Response("Bad Request :^O", { status: 400 });
+    return new Response("Bad Request :^O: " + keys, {
+      status: 400,
+    });
   }
   let authorized = false;
   try {
@@ -38,18 +63,6 @@ export async function onRequestPost(context) {
       authorized = true;
     } else {
       // otherwise, time to check with indieauth's token endpoint
-      // token can either come as a field in the body (access_token=>XXXXXXX)
-      // or in the Authorization header (Bearer XXXXXXXXXX)
-      let token = "";
-      const formDataToken = formData.get("access_token");
-      const headerToken = headers.get("Authorization");
-      token = formDataToken ?? "";
-      if (!token && headerToken.length) {
-        const splitArr = headerToken.split("Bearer ");
-        if (splitArr.length > 1) {
-          token = splitArr[1];
-        }
-      }
       if (token) {
         authorized = await authorizationTokenVerification(token);
       } else {
@@ -81,13 +94,31 @@ export async function onRequestPost(context) {
 
     try {
       const title = formData.get("mp-slug");
+
+      let photo = await formData.get("photo[]");
+
+      let image_urls = [];
+
+      if (formData.has("photo[]")) {
+        image_urls = await imagesToUrls(env, photo, dateString);
+      }
+
+      throw new Error(image_urls);
+
       const content = formData.get("content");
       const postMd = generatePostMarkdown(title, content);
+
       await githubCommitFromAuthenticatedPost(request, env, postMd, dateString);
     } catch (e) {
-      return new Response("Internal Server Error :^( error: " + e.message, {
-        status: 500,
-      });
+      return new Response(
+        "Internal Server Error :^( error: " +
+          e.message +
+          " line: " +
+          e.lineNumber,
+        {
+          status: 500,
+        },
+      );
     }
     return new Response("Success :^)", {
       status: 200,
---
+++ functions/src/images.js
@@ -0,0 +1,22 @@
+export async function imagesToUrls(env, photo, dateString) {
+  const cloudflare_account_id = env.CLOUDFLARE_ACCOUNT_ID;
+  const cloudflare_images_token = env.CLOUDFLARE_IMAGES_TOKEN;
+  const cloudflare_images_url = `https://api.cloudflare.com/client/v4/accounts/${cloudflare_account_id}/images/v1`;
+
+  const formData = new FormData();
+
+  let urls = [];
+  const photoBytes = await photo.bytes();
+  formData.append("file", new File([photoBytes], dateString + ".png"));
+  const response = await fetch(cloudflare_images_url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${cloudflare_images_token}`,
+    },
+    body: formData,
+  });
+
+  const json = await response.json();
+  urls.push(JSON.stringify(json));
+  return urls;
+}
