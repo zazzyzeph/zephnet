86f456a7d54807faf538c1e2ddade32abbd88908
Zephyr Prusinski <zephyr@zephnet.biz>
2025-07-18 23:29:16 -0400

    updates schema, testing events

--- functions/micropub.js
+++ functions/micropub.js
@@ -118,6 +118,9 @@ export async function onRequestPost(context) {
   if (authorized) {
     // for the markdown filename/post link - make a date in the format YYYY-MM-DD_HH-MM-SS
     // i know this is goofy but i don't like JS's built in date/time formats :^)
+    return new Response(JSON.stringify(body.properties), {
+      status: 404,
+    });
     const date = new Date();
     const dateString = dateStringFromDate(date);
 
--- schema.sql
+++ schema.sql
@@ -1,1 +1,5 @@
 CREATE TABLE posts (filename TEXT NOT NULL UNIQUE, h TEXT NOT NULL, title TEXT NOT NULL, content TEXT NOT NULL, image_url TEXT NOT NULL, alt_text TEXT NOT NULL, category TEXT NOT NULL, start_timestamp TEXT NOT NULL, end_timestamp TEXT NOT NULL, location TEXT NOT NULL);
+
+CREATE TABLE tags (post_id INTEGER NOT NULL, name NOT NULL)
+
+CREATE TABLE images (post_id INTEGER NOT NULL, url NOT NULL, featured INTEGER DEFAULT 0, rank INTEGER DEFAULT 0)
