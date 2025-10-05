1e4bb2cdc8a0b7f09cc5870391ec6683cc24369b
Zephyr Prusinski <zephyr@zephnet.biz>
2025-10-05 15:58:55 -0400

    fixes content generator

--- functions/src/generate_entry_markdown.js
+++ functions/src/generate_entry_markdown.js
@@ -23,7 +23,7 @@ export function generateEntryMarkdown(bodyProps, dateString) {
   let md = `
 ---
 title: "${title}"
-type: "posts"
+type: "post"
 date: "${dateIsoString}"
 featured_image: ${imageVar}
 featured_image_alt: ${altVar}
